c3d64361bb856610baed10b70e45d8cc8bd18f86
ColeyG <cjgeerts@gmail.com>
2020-06-16 20:33:15 -0400

    Update

--- src/main.c
+++ src/main.c
@@ -1,38 +1,46 @@
 #include <time.h>
 #include <stdlib.h>
 
+// Pixel Placer
 void place(volatile unsigned short vram[], int x, int y, int color)
 {
   vram[y * 240 + x] = color;
+}
+
+void line()
+{
+  // TODO: Create line drawer
+}
+
+// Called once on the start of the application
+void initialize(volatile unsigned short vram[])
+{
+  place(vram, rand() % 240, rand() % 160, rand());
+}
+
+// Called consistently
+void update(volatile unsigned short vram[])
+{
+  // place(vram, rand() % 240, rand() % 160, rand());
 }
 
 int main(void)
 {
   // Write into the I/O registers, setting video display parameters.
   volatile unsigned char *ioram = (unsigned char *)0x04000000;
-  ioram[0] = 0x03; // Use video mode 3 (in BG2, a 16bpp bitmap in VRAM)
+  ioram[0] = 0x03; // Assign our "game to video mode 3"
   ioram[1] = 0x04; // Enable BG2 (BG0 = 1, BG1 = 2, BG2 = 4, ...)
 
   // Write pixel colours into VRAM
   volatile unsigned short *vram = (unsigned short *)0x06000000;
-  // vram[80 * 240 + 115] = 0x001F; // X = 115, Y = 80, C = 000000000011111 = R
-  // vram[80 * 240 + 120] = 0x03E0; // X = 120, Y = 80, C = 000001111100000 = G
-  // vram[80 * 240 + 125] = 0x7C00; // X = 125, Y = 80, C = 111110000000000 = B
-
-  // place(vram, 10, 10);
-  // place(vram, 11, 10);
-  // place(vram, 12, 10);
-  // place(vram, 13, 10);
 
   srand(time(NULL));
-  // place(vram, rand() % 240, rand() % 160);
 
-  // Wait forever
+  initialize(vram);
+
   while (1)
   {
-    place(vram, rand() % 240, rand() % 160, 0x001F);
-    place(vram, rand() % 240, rand() % 160, 0x03E0);
-    place(vram, rand() % 240, rand() % 160, 0x7C00);
+    update(vram);
   }
 
   return 0;
