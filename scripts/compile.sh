51d4d4fb28f471a8ba07895e77f337e104c9a669
ColeyG <cjgeerts@gmail.com>
2020-06-18 19:09:37 -0400

    Update Script instructiongs

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -2,7 +2,8 @@
 
 #
 # For a more explained usage of this script, visit [my guide](https://github.com/ColeyG/wiki/blob/master/main/gba-dev.md)
-# You can also pass -r after to instantly run the program via retroarch so long as a core is passed 
+# You can also pass -r after to instantly run the program via retroarch so long as a core is passed to the configuration.
+# Config defaults are passed in 'example-config.config' and can be overwritten in by copying it and naming it 'config.config'.
 #
 
 . config/example-config.config
