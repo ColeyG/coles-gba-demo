227169f0bf2be17d394530dbf061e5412bc4a808
ColeyG <cjgeerts@gmail.com>
2020-06-25 11:19:52 -0400

    Remove Math Deps | Externalize Drawing Methods

---
+++ src/drawing.h
@@ -0,0 +1,97 @@
+#ifndef DRAWING
+#define DRAWING
+
+#include "helpers.h"
+
+void place(volatile unsigned short vram[], int x, int y, int color)
+{
+  vram[y * 240 + x] = color;
+}
+
+// This uses [Bresenham's Line Algo](https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm) for drawing
+void line(volatile unsigned short vram[], int x1, int y1, int x2, int y2, int color)
+{
+  int x, y, dx, dy, dx1, dy1, px, py, xe, ye, i;
+  dx = x2 - x1;
+  dy = y2 - y1;
+  dx1 = absoluteDouble(dx);
+  dy1 = absoluteDouble(dy);
+  px = 2 * dy1 - dx1;
+  py = 2 * dx1 - dy1;
+  if (dy1 <= dx1)
+  {
+    if (dx >= 0)
+    {
+      x = x1;
+      y = y1;
+      xe = x2;
+    }
+    else
+    {
+      x = x2;
+      y = y2;
+      xe = x1;
+    }
+    place(vram, x, y, color);
+    for (i = 0; x < xe; i++)
+    {
+      x = x + 1;
+      if (px < 0)
+      {
+        px = px + 2 * dy1;
+      }
+      else
+      {
+        if ((dx < 0 && dy < 0) || (dx > 0 && dy > 0))
+        {
+          y = y + 1;
+        }
+        else
+        {
+          y = y - 1;
+        }
+        px = px + 2 * (dy1 - dx1);
+      }
+      place(vram, x, y, color);
+    }
+  }
+  else
+  {
+    if (dy >= 0)
+    {
+      x = x1;
+      y = y1;
+      ye = y2;
+    }
+    else
+    {
+      x = x2;
+      y = y2;
+      ye = y1;
+    }
+    place(vram, x, y, color);
+    for (i = 0; y < ye; i++)
+    {
+      y = y + 1;
+      if (py <= 0)
+      {
+        py = py + 2 * dx1;
+      }
+      else
+      {
+        if ((dx < 0 && dy < 0) || (dx > 0 && dy > 0))
+        {
+          x = x + 1;
+        }
+        else
+        {
+          x = x - 1;
+        }
+        py = py + 2 * (dx1 - dy1);
+      }
+      place(vram, x, y, color);
+    }
+  }
+}
+
+#endif
---
+++ src/helpers.h
@@ -0,0 +1,13 @@
+#ifndef HELPERS
+#define HELPERS
+
+double absoluteDouble(double x)
+{
+  if (x < 0)
+  {
+    return (x * -1);
+  }
+  return x;
+}
+
+#endif
