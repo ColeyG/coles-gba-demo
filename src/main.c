b07dd045d6058e27903928a32c9d22f29702e526
ColeyG <cjgeerts@gmail.com>
2020-08-04 23:18:08 -0400

    Extra Draw Routine | Control Addresses

--- src/main.c
+++ src/main.c
@@ -5,6 +5,23 @@
 #define MEM_IO 0x04000000
 #define REG_DISPLAY (*((volatile uint32 *)(MEM_IO)))
 #define REG_DISPLAY_VCOUNT (*((volatile uint32 *)(MEM_IO + 0x0006)))
+#define REG_KEY_INPUT (*((volatile uint32 *)(MEM_IO + 0x0130)))
+
+#define KEY_A 0x0001
+#define KEY_B 0x0002
+
+#define KEY_SELECT 0x0004
+#define KEY_START 0x0008
+
+#define KEY_RIGHT 0x0050
+#define KEY_LEFT 0x0060
+#define KEY_UP 0x0070
+#define KEY_DOWN 0x0080
+
+#define KEY_R 0x0100
+#define KEY_L 0x0100
+
+#define KEY_ANY 0x03FF
 
 typedef unsigned int uint32;
 
@@ -19,13 +36,18 @@ void initialize(volatile unsigned short vram[])
   // Initializer Function called on game start
   clearRoutine(vram, 0x739c);
   drawRoutine(vram, coleRoutine, sizeof(coleRoutine) / sizeof(coleRoutine[0]), coleMap, 10, 10);
+  drawRoutine(vram, lnr, sizeof(lnr) / sizeof(lnr[0]), lnrMap, 100, 100);
   addDontDraw(10, 10, 77, 30);
 }
 
 // Called consistently
-void update(volatile unsigned short vram[])
+void update(volatile unsigned short vram[], int keyStates)
 {
   // line(vram, rand() % width, rand() % height, rand() % width, rand() % height, rand());
+  if (keyStates & KEY_UP)
+  {
+    line(vram, rand() % width, rand() % height, rand() % width, rand() % height, rand());
+  }
 }
 
 int main(void)
@@ -35,22 +57,27 @@ int main(void)
   ioram[1] = 0x04;
 
   volatile unsigned short *vram = (unsigned short *)0x06000000;
+  uint32 keyStates = 0;
 
   srand(0);
 
   while (1)
   {
+    // TODO: Currently only works with Vblank, add Hblank
     while (REG_DISPLAY_VCOUNT >= 160)
       ;
     while (REG_DISPLAY_VCOUNT < 160)
     {
+
       if (initialized == 0)
       {
         initialize(vram);
         initialized = 1;
       }
 
-      update(vram);
+      keyStates = ~REG_KEY_INPUT & KEY_ANY;
+
+      update(vram, keyStates);
     }
   }
 
